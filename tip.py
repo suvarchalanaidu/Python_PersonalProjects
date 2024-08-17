5481b872bac37284d918dc657b192572a668a2e5
suvarchalanaidu <110436743+suvarchalanaidu@users.noreply.github.com>
2024-08-16 20:43:22 -0400

    Add files via upload
    Problem_Set_0

---
+++ tip.py
@@ -0,0 +1,28 @@
+#implement two functions:
+
+#dollars_to_float, which should accept a str as input 
+# (formatted as $##.##, wherein each # is a decimal digit), 
+# remove the leading $, and return the amount as a float. For instance, given $50.00 as input, it should return 50.0.
+#percent_to_float, which should accept a str as input 
+# (formatted as ##%, wherein each # is a decimal digit), 
+# remove the trailing %, and return the percentage as a float. For instance, given 15% as input, it should return 0.15.
+
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    removecurrency = float(d.replace("$"," "))
+    return removecurrency
+
+
+def percent_to_float(p):
+    removetrailing = float(p.replace("%",""))
+    p = float(removetrailing/100)
+    return p
+
+
+main()
